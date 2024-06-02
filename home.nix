411f0db96fadbd50eab98f73dc338c96807ff7e6
MaximUltimatum <mcder018@live.com>
2024-06-02 09:42:14 -0500

    Signing on pangolin

--- home.nix
+++ home.nix
@@ -22,30 +22,69 @@
     extraConfig = ''set tabstop=2 expandtab shiftwidth=2 nocompatible clipboard+=unnamedplus'';
   };
 
+  # Configure git diffs, signing, and ignores
   programs.git = {
     enable = true;
+    ignores = [
+      "*.swp"
+      "*~"
+    ];
+    hooks = {}; # we should add a code formatter later
+    # It is safe to commit the KEY ID to git. Just not the private key
+    # signing.key = "";
     signing.signByDefault = true;
-    signing.key = "29E74C433268C88E";
+    signing.key = "1DE0258EDEA3687C02B410000E5996F02A636F3E";
     userEmail = "mcder018@live.com";
     userName = "MaximUltimatum";
+    difftastic.enable = true;
+    difftastic.background = "dark";
+    difftastic.display = "inline";
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+    };
   };
+
+  # If you make changes to this, remember to run `pkill gpg-agent` after your reload your config
+  services.gpg-agent = {
+    enable = true;
+    enableBashIntegration = true;
+    enableSshSupport = true;
+    defaultCacheTtl = 34560000;
+    pinentryPackage = pkgs.pinentry-curses;
+  };
+
 
   # run: `dconf watch /` to find these
   dconf.settings = {
+    "org/gnome/desktop/sound" = {
+      event-sounds = false;
+    };
     "org/gnome/mutter" = {
       workspaces-only-on-primary = false;
     };
-
-    "org/gnome/shell/extensions/ding" = {
-      show-home = false;
-    };
-
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
       gtk-theme = "Yaru-sage-dark";
       icon-theme = "Yaru-sage";
+      enable-hot-corners = false;
     };
-
+    "org/gnome/desktop/peripherals/touchpad" = {
+      click-method = "fingers";
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      ambient-enabled = false;
+    };
+    "org/gnome/settings-daemon/plugins/power" = {
+      idle-dim = true;
+    };
+    "org/gnome/shell/app-switcher" = {
+      current-workspace-only = true;
+    };
+    "org/gnome/shell/extensions/ding" = {
+      show-home = false;
+    };
     "org/gnome/shell/extensions/dash-to-dock" = {
       show-mounts = false;
       show-trash = false;
@@ -53,7 +92,6 @@
       show-show-apps-button = false;
       favorite-apps = [];
     };
-
     "org/gnome/shell/extensions/vitals" = {
       hot-sensors = [
         "_memory_usage_"
@@ -67,36 +105,38 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    # DnD
+    pkgs.google-chrome
+    pkgs.obsidian
+    # pkgs.nextcloud-client
+
+    # development
     pkgs.gh
-    pkgs.tailscale
+    pkgs.xclip
+    pkgs.jetbrains-toolbox
+
+    # customization
     pkgs.gnome.gnome-tweaks
     pkgs.sl
-    pkgs.gnomeExtensions.openweather
-    pkgs.gnomeExtensions.vitals
-    pkgs.gnomeExtensions.dash-to-dock
-    pkgs.xclip
+    pkgs.gnome-extension-manager
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # Version is too new
+    # pkgs.gnomeExtensions.openweather
+    # pkgs.gnomeExtensions.vitals
+    # pkgs.gnomeExtensions.dash-to-dock
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    (pkgs.writeShellScriptBin "fix-pinentry" ''
+      pkill -f gpg-agent; pkill -f pinentry
+      systemctl --user restart gpg-agent{.socket,-extra.socket,-ssh.socket}
+    '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+    # See https://github.com/JetBrains/ideavim/wiki/NERDTree-support
+    ".ideavimrc".text = ''
+      Plug 'preservim/nerdtree'
+      map <C-f> :NERDTreeFocus<CR>
+    '';
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
@@ -117,7 +157,6 @@
   #  /etc/profiles/per-user/veymax/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
